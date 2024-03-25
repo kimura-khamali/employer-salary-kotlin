faa20e924a9a634c365474a1aed550062b468423
kimura-khamali <kimurakhamali@gmail.com>
2024-03-25 09:06:45 +0300

    returns in employers

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+import kotlin.time.Duration
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    employees(hours = 1100)
+}
+
+fun employees(hours: Int ): Int {
+    var numOfHours = hours
+    println(numOfHours)
+
+    return numOfHours
+
+}
+
